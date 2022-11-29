54e08d11cb4017c3abdb0bc085b422ed361c0370
chap0lin <carlosrochacaar@gmail.com>
2022-11-28 21:47:38 -0300

    rafa update

--- calendar.js
+++ calendar.js
@@ -22,7 +22,7 @@ const data = [
   {
     name: "Rafa",
     color: "#800080",
-    data:{"126":true,"127":true,"128":true,"129":true,"130":true,"133":true,"134":true,"135":true,"136":true,"137":true,"140":true,"141":true,"142":true,"143":true,"144":true,"147":true,"148":true,"149":true,"150":true,"151":true,"154":true,"155":true,"156":true,"157":true,"158":true,"161":true,"162":true,"163":true,"164":true,"165":true}
+    data:{"140":true,"141":true,"142":true,"143":true,"144":true,"147":true,"148":true,"149":true,"150":true,"151":true,"154":true,"155":true,"156":true,"157":true,"158":true,"161":true,"162":true,"163":true,"164":true,"165":true,"203":true,"204":true,"205":true,"206":true,"207":true,"210":true,"211":true,"212":true,"214":true,"217":true,"218":true,"219":true,"221":true,"224":true,"225":true,"226":true,"228":true,"231":true,"232":true,"233":true,"235":true,"238":true,"239":true,"240":true,"242":true,"245":true,"246":true,"247":true,"248":true,"249":true,"252":true,"253":true,"254":true,"255":true,"256":true,"259":true,"260":true,"261":true,"262":true,"263":true}
   },
 ]
 
